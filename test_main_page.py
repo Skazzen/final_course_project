4493896592278e94dfe0cebe0303af2b484baa79
Anton Kryuchkov <87002385+Skazzen@users.noreply.github.com>
2021-08-09 12:06:38 +0400

    added final version of project files
    page 4 lesson 4.3

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,5 @@
 from .pages.main_page import MainPage
-from .pages.main_page import login_page
+#from .pages.main_page import login_page
 
 
 
@@ -8,8 +8,8 @@ def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу
-    #page.go_to_login_page() # выполняем метод страницы — переходим на страницу логина
-    page.should_be_login_link()
+    login_page = page.go_to_login_page()
+    login_page.should_be_login_page()
 
 #pytest -v --tb=line --language=en test_main_page.py
 
